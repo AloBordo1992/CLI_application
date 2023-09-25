dd1f8f0814ce5144956cbc2d0ad0e02dcacec4ac
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-25 20:24:09 +0200

    push

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-const now = new Date();
-console.log(`Today ${now.getFullYear()}year`);
+import * as contactService from './contacts.js';
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'contacts':
+      const allContacts = await contactService.listContacts();
+      return console.log(allContacts);
+    case 'getById':
+      const contact = await contactService.getContactById(id);
+      return console.log(contact);
+    case 'add':
+      const newContact = await contactService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+    default:
+      console.log('Unknown actio');
+      break;
+  }
+};
+invokeAction({ action: 'cocontacts' });
+// invokeAction({action: "getById", id:"vza2RIzNGIwutCVCs4mCL"})
+// invokeAction({
+//   acti: 'add',
+//   name: 'Yura',
+//   email: 'sfhffg@gmail.com',
+//   phone: '+48 880 982 857',
+// });
